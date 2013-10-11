f07fc8203db8b04759246d36f9199e8159afc6a3
root <root@godzserver.godz.info>
2013-10-10 19:01:32 -0700

    Minor updates to the project sample code

--- sample/server/server.pl
+++ sample/server/server.pl
@@ -1,10 +1,10 @@
 #!/usr/bin/perl
 use IO::Socket;
-use Strict;
+use strict;
 
-$sock = new IO::Socket::INET (
+my $sock = new IO::Socket::INET (
     LocalHost => '',
-    LocalPort => '7071',
+    LocalPort => '1337',
     Proto => 'tcp',
     Listen => 3,
     Reuse => 1
@@ -12,11 +12,13 @@ $sock = new IO::Socket::INET (
 die "Could not create socket: $!\n" unless $sock;
 
 my $client = $sock->accept();
-open FILE, ">test" or die "Can't open: $!";
-while (<$client>) {
-    print FILE $_;
+while ($client) {
+    open FILE, ">test.txt" or die "Can't open: $!";
+    while(<$client>){
+        print FILE $_;
+    }
+    close FILE;
 }
-close FILE;
 
 close($sock);
 
