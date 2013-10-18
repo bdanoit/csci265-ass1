81e3d8cf64b9e306b155aa6be5171f3c5f27e3c8
root <root@godzserver.godz.info>
2013-10-18 10:31:57 -0700

    Storage module is working with sqlite but may need to be revised

--- server/storage/test.pl
+++ server/storage/test.pl
@@ -3,11 +3,80 @@
 # Baleze Danoit
 # CSCI 265
 
+use lib '../';
 use strict;
 use warnings;
-use storage;
+use storage::storage;
 
-my $db = storage->new();
-print $db->getUsers();
-print $db->getPasswords();
-print $db->getPasswordsByUser('Baleze');
+
+my $ontheline;
+open($ontheline, "| cat |") or die ("Not on the line");
+print $ontheline "select * from users;\n";
+
+while(<$ontheline>){
+    print $_;
+}
+
+print $ontheline ".exit\n";
+exit;
+
+
+my $db = storage::storage->new();
+
+print "PRINT PASSWORDS (SHOULD BE EMPTY)\n";
+if($db->passwordsByUser('Baleze')){
+    my $ref = $db->result();
+    foreach(@{ $ref }){
+        my $hash = $_;
+        print "\t".$hash->{'password'}."\n";
+    }
+}
+
+print "CREATE USER BALEZE\n";
+print $db->error() unless $db->addUser('Baleze');
+print $db->error() unless $db->addUser('Baleze');
+
+print "PRINT USERS (SHOULD DISPLAY BALEZE)\n";
+if($db->users()){
+    my $ref = $db->result();
+    foreach(@{ $ref }){
+        my $hash = $_;
+        print "\t".$hash->{'id'}."\n";
+    }
+}
+
+my @passwords = ("pimp","shizzle","barmitzvah");
+print $db->error() unless $db->addPasswordsByUser('Baleze', \@passwords);
+print "CHECK IF PASSWORDS EXIST FOR BALEZE\n";
+if($db->passwordsByUser('Baleze')){
+    my $ref = $db->result();
+    foreach(@{ $ref }){
+        my $hash = $_;
+        print "\t".$hash->{'password'}."\n";
+    }
+}
+
+print "DELETE PASSWORD shizzle FROM BALEZE\n";
+print $db->error() unless $db->deletePasswordByUser('Baleze', 'shizzle');
+if($db->passwordsByUser('Baleze')){
+    my $ref = $db->result();
+    foreach(@{ $ref }){
+        my $hash = $_;
+        print "\t".$hash->{'password'}."\n";
+    }
+}
+
+print "DELETE USER BALEZE\n";
+print $db->error() unless $db->deleteUser('Baleze');
+
+if($db->passwordsByUser('Baleze')){
+    my $ref = $db->result();
+    foreach(@{ $ref }){
+        my $hash = $_;
+        print "\t".$hash->{'password'}."\n";
+    }
+}
+
+#print $db->error() unless $db->deletePasswordByUser('Baleze', '123456');
+
+#print $db->error() unless $db->addPasswordsByUser('Kam', \@passwords);
