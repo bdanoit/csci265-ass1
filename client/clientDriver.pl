796b1337bb56e4a11bae1af6ab70cbd26f1aa941
Justin David Waterhouse <waterhjd@pup12.csci.viu.ca>
2013-10-22 15:52:50 -0700

    New Client Driver

---
+++ client/clientDriver.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use lib '../';
+
+use strict;
+use warnings;
+use Try::Tiny;
+use parseClientRequest::parseTest;
+
+my $parameters = join(' ', @ARGV);
+
+
+try{
+   my $userRequest = parseClientRequest::parseTest->new();
+   $userRequest->parseString($parameters);
+   my %validRequest = (
+      UserName => $userRequest->UserName,
+      UserPassword => $userRequest->Password,
+      Request => $userRequest->Request,
+      FileName => $userRequest->FileName,
+      );
+}   
+catch{
+   my $ex = $_;
+   if(ref($ex) eq "exc::exception"){
+      print "Exception: {".$ex->get_exc_name()."}\n";
+   }
+   else{ print $ex;}
+};
